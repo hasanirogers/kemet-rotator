e87de05252dde52cc01484238485e6c00464faa8
Hasani Rogers <hasani.rogers@gmail.com>
2020-12-13 00:13:01 -0500

    progress on flip effect

--- src/KemetRotator.js
+++ src/KemetRotator.js
@@ -28,13 +28,47 @@ export class KemetRotator extends LitElement {
       .rotator__slide--active {
         opacity: 1;
       }
+
+      /* flip effect */
+
+      :host([effect="flip"]) .rotator {
+        display: inline-block;
+        position: relative;
+
+        perspective: 500;
+      }
+
+      :host([effect="flip"]) .rotator__slide {
+        position: absolute;
+        top: -20px;
+        left: 0;
+
+        opacity: 0;
+
+        transform: rotateX(90deg);
+        transform-origin: 0% 0%;
+      }
+
+      :host([effect="flip"]) .rotator__slide--active {
+        top: 0;
+        opacity: 1;
+
+        transform: rotateX(0deg);
+      }
     `;
   }
 
   static get properties() {
     return {
+      'index': {
+        type: Number
+      },
       'messages': {
         type: Array
+      },
+      'effect': {
+        type: String,
+        reflect: true,
       },
       'rotationSpeed': {
         type: Number,
@@ -50,13 +84,19 @@ export class KemetRotator extends LitElement {
   constructor() {
     super();
 
+    this.index = 0;
     this.messages = [];
+    this.effect = 'fade';
     this.rotationSpeed = 3000;
     this.transitionSpeed = '500ms';
   }
 
   firstUpdated() {
-    setInterval(() => { this.slideSwitch() }, this.rotationSpeed);
+    this.slides = this.shadowRoot.querySelectorAll('.rotator__slide');
+
+    if (this.rotationSpeed > 0) {
+      setInterval(() => { this.nextSlide() }, this.rotationSpeed * 1000);
+    }
   }
 
   render() {
@@ -68,24 +108,24 @@ export class KemetRotator extends LitElement {
   }
 
   makeMessages() {
-    const styles = `transition: opacity ${this.transitionSpeed} ease;`;
+    const styles = `transition: all ${this.transitionSpeed} ease;`;
 
     const messages = this.messages.map((message, index) => {
-      if (index === 0) {
-        return html`<span class="rotator__slide rotator__slide--active" style="${styles}">${unsafeHTML(message)}</span>`;
-      }
-
-      return html`<span class="rotator__slide" style="${styles}">${unsafeHTML(message)}</span>`;
+      return html`
+        <span class="rotator__slide ${this.index === index ? 'rotator__slide--active' : ''}" style="${styles}">
+          ${unsafeHTML(message)}
+        </span>
+      `;
     });
 
     return messages;
   }
 
-  slideSwitch() {
-    const activeSlide = this.shadowRoot.querySelector('.rotator__slide--active');
-    const nextSlide = activeSlide.nextElementSibling ? activeSlide.nextElementSibling : this.shadowRoot.querySelector('.rotator__slide');
-
-    nextSlide.classList.add('rotator__slide--active');
-    activeSlide.classList.remove('rotator__slide--active');
+  nextSlide() {
+    if (this.index < this.messages.length - 1) {
+      this.index += 1;
+    } else {
+      this.index = 0;
+    }
   }
 }
